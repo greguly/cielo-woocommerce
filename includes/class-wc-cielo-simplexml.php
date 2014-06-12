d29e5be6a522617ac05b4a8875c163ca3ca7a254
claudiosmweb <contato@claudiosmweb.com>
2014-06-12 01:20:21 -0300

    created the WC_PagSeguro_SimpleXML class

---
+++ includes/class-wc-cielo-simplexml.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Extends the SimpleXMLElement class to add CDATA element.
+ */
+class WC_Cielo_SimpleXML extends SimpleXMLElement {
+
+	/**
+	 * Add CDATA.
+	 *
+	 * @param string $string Some string.
+	 */
+	public function addCData( $string ) {
+		$node = dom_import_simplexml( $this );
+		$no   = $node->ownerDocument;
+
+		$node->appendChild( $no->createCDATASection( $string ) );
+	}
+}
