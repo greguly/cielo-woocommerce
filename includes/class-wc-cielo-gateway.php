35d08fb73ca21a4a82f862d5e374c5453ae3b65b
claudiosmweb <contato@claudiosmweb.com>
2014-06-22 03:19:05 -0300

    fixed the admin scripts for woocommerce 2.0.x

--- includes/class-wc-cielo-gateway.php
+++ includes/class-wc-cielo-gateway.php
@@ -329,7 +329,7 @@ class WC_Cielo_Gateway extends WC_Payment_Gateway {
 	 * @return void
 	 */
 	public function admin_scripts( $hook ) {
-		if ( in_array( $hook, array( 'woocommerce_page_wc-settings', 'woocommerce_page_woocommerce_settings' ) ) && ( isset( $_GET['section'] ) && 'wc_cielo_gateway' == $_GET['section'] ) ) {
+		if ( in_array( $hook, array( 'woocommerce_page_wc-settings', 'woocommerce_page_woocommerce_settings' ) ) && ( isset( $_GET['section'] ) && in_array( $_GET['section'], array( 'wc_cielo_gateway', 'WC_Cielo_Gateway' ) ) ) ) {
 			$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
 			wp_enqueue_script( 'wc-cielo-admin', plugins_url( 'assets/js/admin' . $suffix . '.js', plugin_dir_path( __FILE__ ) ), array( 'jquery' ), WC_Cielo::VERSION, true );
