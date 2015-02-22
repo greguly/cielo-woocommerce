7211ec034db574348d1338fa693872d169fabd77
Claudio Sanches <contato@claudiosmweb.com>
2015-02-22 01:36:05 -0300

    Updated the admin message

--- includes/views/html-admin-page.php
+++ includes/views/html-admin-page.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php if ( apply_filters( 'cielo_woocommerce_help_message', true ) ) : ?>
 	<div class="updated woocommerce-message">
-		<p><?php printf( __( 'Help us to implement the Cielo Checkout and Webservice Solution. We count on your help in the form of a %s.', 'cielo-woocommerce' ), '<a href="http://www10.vakinha.com.br/VaquinhaE.aspx?e=342130" target="_blank">' . __( 'Vakinha', 'cielo-woocommerce' ) . '</a>' ); ?></p>
+		<p><?php printf( __( 'Help us to implement the Cielo Checkout. We count on your help in the form of a %s.', 'cielo-woocommerce' ), '<a href="http://www10.vakinha.com.br/VaquinhaE.aspx?e=342130" target="_blank">' . __( 'Vakinha', 'cielo-woocommerce' ) . '</a>' ); ?></p>
 	</div>
 <?php endif; ?>
 
