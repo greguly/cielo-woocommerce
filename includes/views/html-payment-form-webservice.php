1817b3c04a65ceda588bfe183ba46f742400c0c5
Paulo Vieira <paulo.vieira@agenciavivavida.com.br>
2015-03-24 21:18:01 -0300

    correct installment value calculation
    Corrected the value of the installment calculation to reflect the
    interest rate (a.m (per month)) and added the total amount of the final
    payment to make the installment text clearer.
    
    this fixes issue #7

--- includes/views/html-payment-form-webservice.php
+++ includes/views/html-payment-form-webservice.php
@@ -39,17 +39,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 					<option value="0" class="cielo-debit" data-debit="<?php echo esc_attr( $this->debit_methods ); ?>"><?php echo sprintf( __( 'Debit %s%s', 'cielo-woocommerce' ), sanitize_text_field( woocommerce_price( $debit_total ) ), $debit_discount ); ?></option>
 
 				<?php endif; ?>
-				<?php for ( $i = 1; $i <= $this->installments; $i++ ) :
+				<?php 
+ 					for ( $i = 1; $i <= $this->installments; $i++ ) :
+
+ 						$interest_rate = WC_Cielo_API::get_valid_value( $this->interest_rate )/100;
+ 						$financial_index = $interest_rate / (1- (1/pow((1+$interest_rate),$i)));
+
 						$credit_total    = $cart_total / $i;
-						$credit_interest = __( '(no interest)', 'cielo-woocommerce' );
+						$credit_interest = sprintf(__( 'no interest Total: %s', 'cielo-woocommerce' ),sanitize_text_field( woocommerce_price( $cart_total ) ));
 						$smallest_value  = ( 5 <= $this->smallest_installment ) ? $this->smallest_installment : 5;
 
 						if ( 'client' == $this->installment_type && $i >= $this->interest ) {
-							$interest_total = $credit_total * ( ( 100 + WC_Cielo_API::get_valid_value( $this->interest_rate ) ) / 100 );
+							$interest_total = $cart_total * $financial_index; ;//( ( 100 + WC_Cielo_API::get_valid_value( $this->interest_rate ) ) / 100 );
+							$interest_cart_total = $interest_total*$i;
 
 							if ( $credit_total < $interest_total ) {
 								$credit_total    = $interest_total;
-								$credit_interest = sprintf(__( '(with %s%% of interest)', 'cielo-woocommerce' ),(WC_Cielo_API::get_valid_value( $this->interest_rate )));
+								$credit_interest = sprintf(__( '(with interest of %s%% a.m. Total: %s)', 'cielo-woocommerce' ),(WC_Cielo_API::get_valid_value( $this->interest_rate )),sanitize_text_field( woocommerce_price( $interest_cart_total ) ));
 							}
 						}
 
@@ -58,7 +64,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 						}
 					?>
 					<?php if(1==$i){ ?>
-						<option value="<?php echo $i; ?>" class="<?php echo ( 1 == $i ) ? 'cielo-at-sight' : ''; ?>"><?php echo sprintf( __( 'Credit Card %s', 'cielo-woocommerce' ), sanitize_text_field( woocommerce_price( $credit_total ) ) ); ?></option>
+						<option value="<?php echo $i; ?>" class="<?php echo ( 1 == $i ) ? 'cielo-at-sight' : ''; ?>"><?php echo sprintf( __( 'Credit Card %s', 'cielo-woocommerce' ), sanitize_text_field( woocommerce_price( $interest_cart_total ) ) ); ?></option>
 					<?php }else if($i>1){ ?>
 					<option value="<?php echo $i; ?>"><?php echo sprintf( __( '%sx of %s %s', 'cielo-woocommerce' ), $i, sanitize_text_field( woocommerce_price( $credit_total ) ), $credit_interest ); ?></option>
 					<?php } ?>
