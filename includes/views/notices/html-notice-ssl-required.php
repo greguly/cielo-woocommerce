ed73670fbffea0504951d4d548b6d7042abb3a02
Claudio Sanches <contato@claudiosmweb.com>
2015-04-25 12:31:17 -0300

    Improved the ssl required message

--- includes/views/notices/html-notice-ssl-required.php
+++ includes/views/notices/html-notice-ssl-required.php
@@ -10,6 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="error">
-	<p><strong><?php _e( 'Cielo WooCommerce Disabled', 'cielo-woocommerce' ); ?></strong>: <?php _e( 'A SSL Certificate is required for Webservice Solution. Please verify if a certificate is installed on your server.', 'cielo-woocommerce' ); ?>
+	<p><strong><?php _e( 'Cielo WooCommerce Disabled', 'cielo-woocommerce' ); ?></strong>: <?php printf( __( 'A SSL Certificate is required for Webservice Solution. Please verify if a certificate is installed on your server and enable the %s option.', 'cielo-woocommerce' ), '<a href="' . esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section' ) ) . '">' . __( 'Force secure checkout', 'cielo-woocommerce' ) . '</a>' ); ?>
 	</p>
 </div>
