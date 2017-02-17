88d5fb2184c3a946b29c18033092331e88bd88ad
Sander Mendes <sandermendes@gmail.com>
2017-02-16 23:19:26 -0200

    Check if status sale already captured.

--- includes/views/admin/class-wc-cielo-admin-orders.php
+++ includes/views/admin/class-wc-cielo-admin-orders.php
@@ -27,7 +27,7 @@ class WC_Cielo_Admin_Orders {
     }
 
     /**
-     * Register tracking code metabox.
+     * Register capture sale metabox.
      */
     public function register_metabox( ) {
 
@@ -37,7 +37,7 @@ class WC_Cielo_Admin_Orders {
         if ( $order->payment_method == 'cielo_credit' ) {
             add_meta_box(
                 'wc_cielo',
-                'Cielo - Capture Sale',
+                __( 'Cielo - Capture Sale', 'cielo-woocommerce' ),
                 array($this, 'metabox_content'),
                 'shop_order',
                 'side',
@@ -48,32 +48,54 @@ class WC_Cielo_Admin_Orders {
     }
 
     /**
-     * Tracking code metabox content.
+     * Capture sale metabox content.
      *
      * @param WC_Post $post Post data.
      */
     public function metabox_content( $post ) {
-
+        // Order
         $order     = wc_get_order( $post->ID );
 
+        // Difference between date of order and present days
         $diff  = ( strtotime( $order->order_date ) - strtotime( current_time( 'mysql' ) ) );
         $days  = absint( $diff / ( 60 * 60 * 24 ) );
-        $limit = 15;
+        $limit = maybe_unserialize(get_option('woocommerce_cielo_general_settings_settings'))['time_sale_capture'];
 
-        echo '<label style="display: '. ( get_post_meta( $post->ID, '_cielo_sale_captured_status', true ) == "yes" ? 'none' : 'block' ) .';">'. sprintf( __( '%s day(s) left', 'cielo-woocommerce' ), ($limit - $days) ) .'</label>';
-        echo '<ul class="cielo_action submitbox" style="display: flex;">' .
-             '   <li class="wide" id="actions" style="width: 100%">' .
-             '       <input type="text" id="capture_amount" name="capture_amount" value="' . esc_attr( wc_format_localized_price( $order->get_total() ) ) . '"  class="wc_input_price regular_price" ' . disabled( get_post_meta( $post->ID, '_cielo_sale_captured_status', true ), "yes", false ) . ' />'.
-             '   </li>' .
-             '   <li class="wide">' .
-             '       <input type="submit" class="button tips" id="cielo_sale_captured" name="cielo_sale_captured" value="' . ( ( get_post_meta( $post->ID, '_cielo_sale_captured_status', true ) == "yes" ) ? "Captured" : "Capture" ) . '" data-tip="'. __( "Capture sale to finalize and charge transaction", "woocommerce" ) . '" ' . disabled( get_post_meta( $post->ID, '_cielo_sale_captured_status', true ), "yes", false ) . ' />' .
-             '   </li>' .
-             '</ul>';
+        $captured_status = get_post_meta($post->ID, '_cielo_sale_captured_status', true);
+
+        if ($captured_status != "yes") {
+
+            if ($limit > $days) {
+
+                echo '<label>' . sprintf(__('%s day(s) left', 'cielo-woocommerce'), ($limit - $days)) . '</label>';
+                echo '<ul class="cielo_action submitbox" style="display: flex;">' .
+                    '   <li class="wide" id="actions" style="width: 100%">' .
+                    '       <input type="number" step="0.01" min="0" max="' . esc_attr($order->get_total()) . '" id="capture_amount" name="capture_amount" value="' . esc_attr($order->get_total()) . '"  class="wc_input_price regular_price" ' . disabled($captured_status, "yes", false) . ' style="width: 90%" />' .
+                    '   </li>' .
+                    '   <li class="wide">' .
+                    '       <input type="submit" class="button tips" id="cielo_sale_captured" name="cielo_sale_captured" onclick="return confirm(\''. __('Do you right about proceed with that capture? This operation cannot be undone.', 'cielo-woocommerce') . '\')" value="' . ((get_post_meta($post->ID, '_cielo_sale_captured_status', true) == "yes") ? __("Captured", "cielo-woocommerce") : __("Capture", "cielo-woocommerce")) . '" data-tip="' . __("Capture sale to finalize and charge transaction", "cielo-woocommerce") . '" ' . disabled($captured_status, "yes", false) . ' />' .
+                    '   </li>' .
+                    '</ul>';
+
+            } else {
+
+                echo '<label>' . __('Term to capture sale exceeded', 'cielo-woocommerce') . '</label>';
+
+            }
+
+        } else {
+            echo '<label>' . __('Captured', 'cielo-woocommerce') . '</label>';
+            echo
+                '<div>' .
+                '    <input type="number" step="0.01" min="0" max="' . esc_attr($order->get_total()) . '" id="capture_amount" name="capture_amount" value="' . $order->get_total() . '"  class="wc_input_price regular_price" style="width: 100%" disabled />' .
+                '</div>';
+
+        }
 
     }
 
     /**
-     * Save tracking code.
+     * Do action capture sale.
      *
      * @param int $post_id Current post type ID.
      */
@@ -83,23 +105,21 @@ class WC_Cielo_Admin_Orders {
             return;
         }
 
-        if ( isset( $_POST['cielo_sale_captured'] ) ) {
+        // VErify if have a value
+        if ( isset( $_POST['capture_amount'] ) ) {
 
-//            $tracking_code = sanitize_text_field( $tracking_code );
+            //Get order
+            $order = wc_get_order( $post_id );
 
-            $order     = wc_get_order( $post_id );
-            echo str_replace('"', '', $order->get_transaction_id());
-
-            echo 'Order Date: ' . $order->order_date;
-            
+            //Create a instance of Credit Gateway
             $cielo_credit = new WC_Cielo_Credit_Gateway();
 
+            //Create a instance of Credit API
             $api = new WC_Cielo_API( $cielo_credit );
 
+            //Sale Capture
             try {
-
-                $response = $api->do_sale_capture( $order, '', $order->get_transaction_id(), $_POST['cielo_sale_captured'] );
-
+                $response = $api->do_sale_capture( $order, '', $order->get_transaction_id(), esc_attr($_POST ['capture_amount'] ) );
             } catch (CieloRequestException $e) {
                 $error = $e->getCieloError();
             }
