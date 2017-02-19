b230b983b760c7b114c2d6a97ff1d60818041b1c
Sander Mendes <sandermendes@gmail.com>
2017-02-19 09:57:24 -0300

    Implemented Direct Debit frontend Gateway

--- templates/direct-debit/webservice-payment-form.php
+++ templates/direct-debit/webservice-payment-form.php
@@ -11,28 +11,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<fieldset id="cielo-directdebit-payment-form" class="cielo-payment-form">
+<fieldset id="cielo-directdebit-payment-form" class="cielo-payment-form" style="display: block;">
 
 	<p><?php _e( 'Select your bank:', 'cielo-woocommerce' ); ?></p>
-	<ul>
-		<li>
-			<label>
-				<input type="radio" name="cielo_direct_debit" value="bradescodebit" />
-				<i id="cielo-icon-bradesco"></i>
+	<ul style="list-style: none; margin: 0 0 10px; padding: 0;">
+        <style type="text/css">
+            input[name="cielo_direct_debit"]:checked {
+                outline: 1px solid red
+            }
+        </style>
+		<li style="border: none; display: block; float: left; list-style: none; margin: 0; padding: 0; width: 130px;">
+			<label style="cursor: pointer; display: block; font-weight: bold; margin: 0; padding: 5px 10px; text-align: center;">
+				<input type="radio" name="cielo_direct_debit" value="Bradesco" style="display: none;" />
+				<i id="cielo-icon-bradesco" style="background-color: transparent; background-image: url(https://francompras.com.br/wp-content/plugins/cielo-woocommerce/assets/images/transparent-checkout-icons.png); background-repeat: no-repeat; display: block; height: 51px; margin: 0 auto; width: 51px;"></i>
 				<span>
 					<?php _e( 'Bradesco', 'cielo-woocommerce' ); ?>
 				</span>
 			</label>
 		</li>
-		<li>
-			<label>
-				<input type="radio" name="cielo_direct_debit" value="bancodobrasildebit" />
-				<i id="cielo-icon-bancodobrasil"></i>
+		<li style="border: none; display: block; float: left; list-style: none; margin: 0; padding: 0; width: 130px;">
+			<label style="cursor: pointer; display: block; font-weight: bold; margin: 0; padding: 5px 10px; text-align: center;">
+				<input type="radio" name="cielo_direct_debit" value="Banco do Brasil" style="display: none;" />
+				<i id="cielo-icon-bancodobrasil" style="background-color: transparent; background: url(https://francompras.com.br/wp-content/plugins/cielo-woocommerce/assets/images/transparent-checkout-icons.png) 20% 0; background-repeat: no-repeat; display: block; height: 51px; margin: 0 auto; width: 51px;"></i>
 				<span>
 					<?php _e( 'Banco do Brasil', 'cielo-woocommerce' ); ?></span>
 			</label>
 		</li>
 	</ul>
-	<p><?php _e( '* After clicking "Proceed to payment" you will have access to the link that will take you to your bank\'s website, so you can make the payment in total security.', 'cielo-woocommerce' ); ?></p>
+    <br>
+	<p style="margin-bottom: .5em; padding: 100px 0 0 0;"><?php _e( '* After clicking "Proceed to payment" you will have access to the link that will take you to your bank\'s website, so you can make the payment in total security.', 'cielo-woocommerce' ); ?></p>
 	<div class="clear"></div>
 </fieldset>
