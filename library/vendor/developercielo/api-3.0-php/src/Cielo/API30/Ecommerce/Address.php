1c101ec607f8feb685f45e5d32cfa203402ae2b6
Sander Mendes <sandermendes@gmail.com>
2017-05-31 13:42:40 -0300

    Update library Cielo API 3.0

--- library/vendor/developercielo/api-3.0-php/src/Cielo/API30/Ecommerce/Address.php
+++ library/vendor/developercielo/api-3.0-php/src/Cielo/API30/Ecommerce/Address.php
@@ -1,30 +1,26 @@
 <?php
+
 namespace Cielo\API30\Ecommerce;
 
-class Address implements CieloSerializable
-{
+class Address implements CieloSerializable {
 
     private $street;
-
     private $number;
-
     private $complement;
-
     private $zipCode;
-
     private $city;
-
     private $state;
-
     private $country;
+    private $district;
 
-    public function jsonSerialize()
-    {
+    public function jsonSerialize() {
+
         return get_object_vars($this);
+
     }
 
-    public function populate(\stdClass $data)
-    {
+    public function populate(\stdClass $data) {
+
         $this->street = isset($data->Street)? $data->Street: null;
         $this->number = isset($data->Number)? $data->Number: null;
         $this->complement = isset($data->Complement)? $data->Complement: null;
@@ -32,82 +28,112 @@ class Address implements CieloSerializable
         $this->city = isset($data->City)? $data->City: null;
         $this->state = isset($data->State)? $data->State: null;
         $this->country = isset($data->Country)? $data->Country: null;
+        $this->district = isset($data->District)? $data->District: null;
+
     }
 
-    public function getStreet()
-    {
+    public function getDistrict() {
+
+        return $this->district;
+
+    }
+
+    public function setDistrict($district) {
+
+        $this->district = $district;
+        return $this;
+
+    }
+
+    public function getStreet() {
+
         return $this->street;
+
     }
 
-    public function setStreet($street)
-    {
+    public function setStreet($street) {
+
         $this->street = $street;
         return $this;
+
     }
 
-    public function getNumber()
-    {
+    public function getNumber() {
+
         return $this->number;
+
     }
 
-    public function setNumber($number)
-    {
+    public function setNumber($number) {
+
         $this->number = $number;
         return $this;
+
     }
 
-    public function getComplement()
-    {
+    public function getComplement() {
+
         return $this->complement;
+
     }
 
-    public function setComplement($complement)
-    {
+    public function setComplement($complement) {
+
         $this->complement = $complement;
         return $this;
+
     }
 
-    public function getZipCode()
-    {
+    public function getZipCode() {
+
         return $this->zipCode;
+
     }
 
-    public function setZipCode($zipCode)
-    {
+    public function setZipCode($zipCode) {
+
         $this->zipCode = $zipCode;
         return $this;
+
     }
 
-    public function getCity()
-    {
+    public function getCity() {
+
         return $this->city;
+
     }
 
-    public function setCity($city)
-    {
+    public function setCity($city) {
+
         $this->city = $city;
         return $this;
+
     }
 
-    public function getState()
-    {
+    public function getState() {
+
         return $this->state;
+
     }
 
-    public function setState($state)
-    {
+    public function setState($state) {
+
         $this->state = $state;
         return $this;
+
     }
 
-    public function getCountry()
-    {
+    public function getCountry() {
+
         return $this->country;
+
     }
 
-    public function setCountry($country)
-    {
+    public function setCountry($country) {
+
         $this->country = $country;
         return $this;
+
     }
+
 }
--- library/vendor/developercielo/api-3.0-php/src/Cielo/API30/Ecommerce/Payment.php
+++ library/vendor/developercielo/api-3.0-php/src/Cielo/API30/Ecommerce/Payment.php
@@ -1,106 +1,73 @@
 <?php
+
 namespace Cielo\API30\Ecommerce;
 
-class Payment implements \JsonSerializable
-{
+class Payment implements \JsonSerializable {
 
     const PAYMENTTYPE_CREDITCARD = 'CreditCard';
-
     const PAYMENTTYPE_DEBITCARD = 'DebitCard';
-
     const PAYMENTTYPE_ELECTRONIC_TRANSFER = 'ElectronicTransfer';
-
     const PAYMENTTYPE_BOLETO = 'Boleto';
-
     const PROVIDER_BRADESCO = 'Bradesco';
-
     const PROVIDER_BANCO_DO_BRASIL = 'BancoDoBrasil';
-
     const PROVIDER_SIMULADO = 'Simulado';
 
     private $serviceTaxAmount;
-
     private $installments;
-
     private $interest;
-
     private $capture = false;
-
     private $authenticate = false;
-
     private $recurrent;
-
     private $recurrentPayment;
-
     private $creditCard;
-
     private $debitCard;
-
     private $authenticationUrl;
-
     private $tid;
-
     private $proofOfSale;
-
     private $authorizationCode;
-
     private $softDescriptor = "";
-
     private $returnUrl;
-
     private $provider;
-
     private $paymentId;
-
     private $type;
-
     private $amount;
-
     private $receivedDate;
-
     private $capturedAmount;
-
     private $capturedDate;
-
     private $currency;
-
     private $country;
-
     private $returnCode;
-
     private $returnMessage;
-
     private $status;
-
     private $links;
-
     private $extraDataCollection;
-
     private $expirationDate;
-
     private $url;
-
     private $number;
-
+    private $boletoNumber;
     private $barCodeNumber;
-
     private $digitableLine;
-
     private $address;
+    private $assignor;
+    private $demonstrative;
+    private $identification;
 
-    public function __construct($amount = 0, $installments = 1)
-    {
+    private $instructions;
+
+    public function __construct($amount = 0, $installments = 1) {
+
         $this->setAmount($amount);
         $this->setInstallments($installments);
+
     }
 
-    public function jsonSerialize()
-    {
+    public function jsonSerialize() {
+
         return get_object_vars($this);
+
     }
 
-    public function populate(\stdClass $data)
-    {
+    public function populate(\stdClass $data) {
 
         $this->serviceTaxAmount = isset($data->ServiceTaxAmount)? $data->ServiceTaxAmount: null;
         $this->installments = isset($data->Installments)? $data->Installments: null;
@@ -124,19 +91,23 @@ class Payment implements \JsonSerializable
             $this->debitCard->populate($data->DebitCard);
         }
 
+        $this->expirationDate  =  isset($data->ExpirationDate)?$data->ExpirationDate: null;
+        $this->url             =  isset($data->Url)?$data->Url: null;
+        $this->boletoNumber    =  isset($data->BoletoNumber)? $data->BoletoNumber: null;
+        $this->barCodeNumber   =  isset($data->BarCodeNumber)?$data->BarCodeNumber: null;
+        $this->digitableLine   =  isset($data->DigitableLine)?$data->DigitableLine: null;
+        $this->address         =  isset($data->Address)?$data->Address: null;
+
         $this->authenticationUrl = isset($data->AuthenticationUrl)? $data->AuthenticationUrl: null;
         $this->tid = isset($data->Tid)? $data->Tid: null;
         $this->proofOfSale = isset($data->ProofOfSale)? $data->ProofOfSale: null;
         $this->authorizationCode = isset($data->AuthorizationCode)? $data->AuthorizationCode: null;
         $this->softDescriptor = isset($data->SoftDescriptor)? $data->SoftDescriptor: null;
-        $this->returnUrl = isset($data->ReturnUrl)? $data->ReturnUrl: null;
         $this->provider = isset($data->Provider)? $data->Provider: null;
         $this->paymentId = isset($data->PaymentId)? $data->PaymentId: null;
         $this->type = isset($data->Type)? $data->Type: null;
         $this->amount = isset($data->Amount)? $data->Amount: null;
         $this->receivedDate = isset($data->ReceivedDate)? $data->ReceivedDate: null;
-        $this->capturedAmount = isset($data->CapturedAmount)? $data->CapturedAmount: null;
-        $this->capturedDate = isset($data->CapturedDate)? $data->CapturedDate: null;
         $this->currency = isset($data->Currency)? $data->Currency: null;
         $this->country = isset($data->Country)? $data->Country: null;
         $this->returnCode = isset($data->ReturnCode)? $data->ReturnCode: null;
@@ -145,449 +116,580 @@ class Payment implements \JsonSerializable
 
         $this->links = isset($data->Links)? $data->Links: [];
 
-        $this->extraDataCollection = isset($data->ExtraDataCollection)? $data->ExtraDataCollection: null;
-        $this->expirationDate = isset($data->ExpirationDate)? $data->ExpirationDate: null;
-        $this->url = isset($data->Url)? $data->Url: null;
-        $this->number = isset($data->Number)? $data->Number: null;
-        $this->barCodeNumber = isset($data->BarCodeNumber)? $data->BarCodeNumber: null;
-        $this->digitableLine = isset($data->DigitableLine)? $data->DigitableLine: null;
-        $this->address = isset($data->Address)? $data->Address: null;
+        $this->assignor = isset($data->Assignor)? $data->Assignor: null;
+        $this->demonstrative = isset($data->Demonstrative)? $data->Demonstrative: null;
+        $this->identification = isset($data->Identification)? $data->Identification: null;
+        $this->instructions = isset($data->Instructions)? $data->Instructions: null;
+
     }
 
-    public static function fromJson($json)
-    {
+    public static function fromJson($json) {
+
         $payment = new Payment();
         $payment->populate(json_decode($json));
-
         return $payment;
+
     }
 
-    private function newCard($securityCode, $brand)
-    {
+    private function newCard($securityCode, $brand) {
+
         $card = new CreditCard();
         $card->setSecurityCode($securityCode);
         $card->setBrand($brand);
-
         return $card;
+
     }
 
-    public function creditCard($securityCode, $brand)
-    {
-        $card = $this->newCard($securityCode, $brand);
+    public function creditCard($securityCode, $brand) {
 
+        $card = $this->newCard($securityCode, $brand);
         $this->setType(self::PAYMENTTYPE_CREDITCARD);
         $this->setCreditCard($card);
-
         return $card;
+
     }
 
-    public function debitCard($securityCode, $brand)
-    {
-        $card = $this->newCard($securityCode, $brand);
+    public function debitCard($securityCode, $brand) {
 
+        $card = $this->newCard($securityCode, $brand);
         $this->setType(self::PAYMENTTYPE_DEBITCARD);
         $this->setDebitCard($card);
-
         return $card;
+
     }
 
-    public function recurrentPayment($authorizeNow = true)
-    {
+    public function recurrentPayment($authorizeNow = true) {
+
         $recurrentPayment = new RecurrentPayment($authorizeNow);
-
         $this->setRecurrentPayment($recurrentPayment);
-
         return $recurrentPayment;
+
     }
 
-    public function getServiceTaxAmount()
-    {
+    public function getServiceTaxAmount() {
+
         return $this->serviceTaxAmount;
+
     }
 
-    public function setServiceTaxAmount($serviceTaxAmount)
-    {
+    public function setServiceTaxAmount($serviceTaxAmount) {
+
         $this->serviceTaxAmount = $serviceTaxAmount;
         return $this;
+
     }
 
-    public function getInstallments()
-    {
+    public function getInstallments() {
+
         return $this->installments;
+
     }
 
-    public function setInstallments($installments)
-    {
+    public function setInstallments($installments) {
+
         $this->installments = $installments;
         return $this;
+
     }
 
-    public function getInterest()
-    {
+    public function getInterest() {
+
         return $this->interest;
+
     }
 
-    public function setInterest($interest)
-    {
+    public function setInterest($interest) {
+
         $this->interest = $interest;
         return $this;
+
     }
 
-    public function getCapture()
-    {
+    public function getCapture() {
+
         return $this->capture;
+
     }
 
-    public function setCapture($capture)
-    {
+    public function setCapture($capture) {
+
         $this->capture = $capture;
         return $this;
+
     }
 
-    public function getAuthenticate()
-    {
+    public function getAuthenticate() {
+
         return $this->authenticate;
+
     }
 
-    public function setAuthenticate($authenticate)
-    {
+    public function setAuthenticate($authenticate) {
+
         $this->authenticate = $authenticate;
         return $this;
+
     }
 
-    public function getRecurrent()
-    {
+    public function getRecurrent() {
+
         return $this->recurrent;
+
     }
 
-    public function setRecurrent($recurrent)
-    {
+    public function setRecurrent($recurrent) {
+
         $this->recurrent = $recurrent;
         return $this;
+
     }
 
-    public function getRecurrentPayment()
-    {
+    public function getRecurrentPayment() {
+
         return $this->recurrentPayment;
+
     }
 
-    public function setRecurrentPayment($recurrentPayment)
-    {
+    public function setRecurrentPayment($recurrentPayment) {
+
         $this->recurrentPayment = $recurrentPayment;
         return $this;
+
     }
 
-    public function getCreditCard()
-    {
+    public function getCreditCard() {
+
         return $this->creditCard;
+
     }
 
-    public function setCreditCard(CreditCard $creditCard)
-    {
+    public function setCreditCard(CreditCard $creditCard) {
+
         $this->creditCard = $creditCard;
         return $this;
+
     }
 
     /**
      * @return mixed
      */
-    public function getDebitCard()
-    {
+    public function getDebitCard() {
+
         return $this->debitCard;
+
     }
 
     /**
      * @param mixed $debitCard
      */
-    public function setDebitCard($debitCard)
-    {
+    public function setDebitCard($debitCard) {
+
         $this->debitCard = $debitCard;
         return $this;
+
     }
 
-    public function getAuthenticationUrl()
-    {
+    public function getAuthenticationUrl() {
+
         return $this->authenticationUrl;
+
     }
 
-    public function setAuthenticationUrl($authenticationUrl)
-    {
+    public function setAuthenticationUrl($authenticationUrl) {
+
         $this->authenticationUrl = $authenticationUrl;
         return $this;
+
     }
 
-    public function getTid()
-    {
+    public function getTid() {
+
         return $this->tid;
+
     }
 
-    public function setTid($tid)
-    {
+    public function setTid($tid) {
+
         $this->tid = $tid;
         return $this;
+
     }
 
-    public function getProofOfSale()
-    {
+    public function getProofOfSale() {
+
         return $this->proofOfSale;
+
     }
 
-    public function setProofOfSale($proofOfSale)
-    {
+    public function setProofOfSale($proofOfSale) {
+
         $this->proofOfSale = $proofOfSale;
         return $this;
+
     }
 
-    public function getAuthorizationCode()
-    {
+    public function getAuthorizationCode() {
+
         return $this->authorizationCode;
+
     }
 
-    public function setAuthorizationCode($authorizationCode)
-    {
+    public function setAuthorizationCode($authorizationCode) {
+
         $this->authorizationCode = $authorizationCode;
         return $this;
+
     }
 
-    public function getSoftDescriptor()
-    {
+    public function getSoftDescriptor() {
+
         return $this->softDescriptor;
+
     }
 
-    public function setSoftDescriptor($softDescriptor)
-    {
+    public function setSoftDescriptor($softDescriptor) {
+
         $this->softDescriptor = $softDescriptor;
         return $this;
+
     }
 
-    public function getReturnUrl()
-    {
+    public function getReturnUrl() {
+
         return $this->returnUrl;
+
     }
 
-    public function setReturnUrl($returnUrl)
-    {
+    public function setReturnUrl($returnUrl) {
+
         $this->returnUrl = $returnUrl;
         return $this;
+
     }
 
-    public function getProvider()
-    {
+    public function getProvider() {
+
         return $this->provider;
+
     }
 
-    public function setProvider($provider)
-    {
+    public function setProvider($provider) {
+
         $this->provider = $provider;
         return $this;
+
     }
 
-    public function getPaymentId()
-    {
+    public function getPaymentId() {
+
         return $this->paymentId;
+
     }
 
-    public function setPaymentId($paymentId)
-    {
+    public function setPaymentId($paymentId) {
+
         $this->paymentId = $paymentId;
         return $this;
+
     }
 
-    public function getType()
-    {
+    public function getType() {
+
         return $this->type;
+
     }
 
-    public function setType($type)
-    {
+    public function setType($type) {
+
         $this->type = $type;
         return $this;
+
     }
 
-    public function getAmount()
-    {
+    public function getAmount() {
+
         return $this->amount;
+
     }
 
-    public function setAmount($amount)
-    {
+    public function setAmount($amount) {
+
         $this->amount = $amount;
         return $this;
+
     }
 
-    public function getReceivedDate()
-    {
+    public function getReceivedDate() {
+
         return $this->receivedDate;
+
     }
 
-    public function setReceivedDate($receivedDate)
-    {
+    public function setReceivedDate($receivedDate) {
+
         $this->receivedDate = $receivedDate;
         return $this;
+
     }
 
-    public function getCapturedAmount()
-    {
+    public function getCapturedAmount() {
+
         return $this->capturedAmount;
+
     }
 
-    public function setCapturedAmount($capturedAmount)
-    {
+    public function setCapturedAmount($capturedAmount) {
+
         $this->capturedAmount = $capturedAmount;
         return $this;
+
     }
 
-    public function getCapturedDate()
-    {
+    public function getCapturedDate() {
+
         return $this->capturedDate;
+
     }
 
-    public function setCapturedDate($capturedDate)
-    {
+    public function setCapturedDate($capturedDate) {
+
         $this->capturedDate = $capturedDate;
         return $this;
+
     }
 
-    public function getCurrency()
-    {
+    public function getCurrency() {
+
         return $this->currency;
+
     }
 
-    public function setCurrency($currency)
-    {
+    public function setCurrency($currency) {
+
         $this->currency = $currency;
         return $this;
+
     }
 
-    public function getCountry()
-    {
+    public function getCountry() {
+
         return $this->country;
+
     }
 
-    public function setCountry($country)
-    {
+    public function setCountry($country) {
+
         $this->country = $country;
         return $this;
+
     }
 
-    public function getReturnCode()
-    {
+    public function getReturnCode() {
+
         return $this->returnCode;
+
     }
 
-    public function setReturnCode($returnCode)
-    {
+    public function setReturnCode($returnCode) {
+
         $this->returnCode = $returnCode;
         return $this;
+
     }
 
-    public function getReturnMessage()
-    {
+    public function getReturnMessage() {
+
         return $this->returnMessage;
+
     }
 
-    public function setReturnMessage($returnMessage)
-    {
+    public function setReturnMessage($returnMessage) {
+
         $this->returnMessage = $returnMessage;
         return $this;
+
     }
 
-    public function getStatus()
-    {
+    public function getStatus() {
+
         return $this->status;
+
     }
 
-    public function setStatus($status)
-    {
+    public function setStatus($status) {
+
         $this->status = $status;
         return $this;
+
     }
 
-    public function getLinks()
-    {
+    public function getLinks() {
+
         return $this->links;
+
     }
 
-    public function setLinks($links)
-    {
+    public function setLinks($links) {
+
         $this->links = $links;
         return $this;
+
     }
 
-    public function getExtraDataCollection()
-    {
+    public function getExtraDataCollection() {
+
         return $this->extraDataCollection;
+
     }
 
-    public function setExtraDataCollection($extraDataCollection)
-    {
+    public function setExtraDataCollection($extraDataCollection) {
+
         $this->extraDataCollection = $extraDataCollection;
         return $this;
+
     }
 
-    public function getExpirationDate()
-    {
+    public function getExpirationDate() {
+
         return $this->expirationDate;
+
     }
 
-    public function setExpirationDate($expirationDate)
-    {
+    public function setExpirationDate($expirationDate) {
+
         $this->expirationDate = $expirationDate;
         return $this;
+
     }
 
-    public function getUrl()
-    {
+    public function getUrl() {
+
         return $this->url;
+
     }
 
-    public function setUrl($url)
-    {
+    public function setUrl($url) {
+
         $this->url = $url;
         return $this;
+
     }
 
-    public function getNumber()
-    {
+    public function getNumber() {
+
         return $this->number;
+
     }
 
-    public function setNumber($number)
-    {
+    public function setNumber($number) {
+
         $this->number = $number;
         return $this;
+
     }
 
-    public function getBarCodeNumber()
-    {
+    public function getBoletoNumber() {
+
+        return $this->boletoNumber;
+
+    }
+
+    public function setBoletoNumber($boletoNumber) {
+
+        $this->boletoNumber = $boletoNumber;
+        return $this;
+
+    }
+
+    public function getBarCodeNumber() {
+
         return $this->barCodeNumber;
+
     }
 
-    public function setBarCodeNumber($barCodeNumber)
-    {
+    public function setBarCodeNumber($barCodeNumber) {
+
         $this->barCodeNumber = $barCodeNumber;
         return $this;
+
     }
 
-    public function getDigitableLine()
-    {
+    public function getDigitableLine() {
+
         return $this->digitableLine;
+
     }
 
-    public function setDigitableLine($digitableLine)
-    {
+    public function setDigitableLine($digitableLine) {
+
         $this->digitableLine = $digitableLine;
         return $this;
+
     }
 
-    public function getAddress()
-    {
+    public function getAddress() {
+
         return $this->address;
+
     }
 
-    public function setAddress($address)
-    {
+    public function setAddress($address) {
+
         $this->address = $address;
         return $this;
+
     }
+
+    public function getAssignor() {
+
+        return $this->assignor;
+
+    }
+
+    public function setAssignor($assignor) {
+
+        $this->assignor = $assignor;
+        return $this;
+
+    }
+
+    public function getDemonstrative() {
+
+        return $this->demonstrative;
+
+    }
+
+    public function setDemonstrative($demonstrative) {
+
+        $this->demonstrative = $demonstrative;
+        return $this;
+
+    }
+
+    public function getIdentification() {
+
+        return $this->identification;
+
+    }
+
+    public function setIdentification($identification) {
+
+        $this->identification = $identification;
+        return $this;
+
+    }
+
+    public function getInstructions() {
+
+        return $this->instructions;
+
+    }
+
+    public function setInstructions($instructions) {
+
+        $this->instructions = $instructions;
+        return $this;
+
+    }
+
 }
