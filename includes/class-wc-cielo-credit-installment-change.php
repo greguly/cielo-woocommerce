b678c4c662c6664433c15dbf2e2b35d0d9fc0485
Sander Mendes <sandermendes@gmail.com>
2018-03-15 14:06:05 -0300

    Update status order to work with Clearsale

--- includes/class-wc-cielo-credit-installment-change.php
+++ includes/class-wc-cielo-credit-installment-change.php
@@ -38,14 +38,6 @@ class WC_Cielo_Credit_Installment_Change {
 
             wp_enqueue_script( 'wc-cielo-credit-installment-change', plugins_url( 'assets/js/credit-card/checkout-installment-change' . $suffix . '.js', plugin_dir_path( __FILE__ ) ), array( 'jquery', 'jquery-blockui' ), WC_Cielo::VERSION, true );
 
-            wp_localize_script(
-                'wc-cielo-credit-installment-change',
-                'WCCieloCreditInstallmentChange',
-                array(
-                    'url'   => WC_AJAX::get_endpoint( $this->ajax_endpoint ),
-                    'discount_nonce' => wp_create_nonce( 'woocommerce_cielo_credit_installment_change_nonce' ),
-                )
-            );
         }
     }
 
@@ -61,50 +53,77 @@ class WC_Cielo_Credit_Installment_Change {
             $post_data = $_POST; // fallback for final checkout (non-ajax)
         }
 
-        if (isset($post_data['cielo_credit_installments'])) {
+        if (WC()->session->chosen_payment_method == 'cielo_credit') {
 
-            $settings = get_option('woocommerce_' . WC()->session->chosen_payment_method . '_settings');
+            if (isset($post_data['cielo_credit_installments'])) {
 
-            $cielo_credit_installments = (int) $post_data['cielo_credit_installments'];
-            $interest = $settings['interest'];
+                $settings = get_option('woocommerce_' . WC()->session->chosen_payment_method . '_settings');
 
-            if ($cielo_credit_installments == 1) {
+                $cielo_credit_installments = (int)$post_data['cielo_credit_installments'];
+                $interest = $settings['interest'];
 
-                if (isset($settings['credit_discount_x1'])) {
+                if ($cielo_credit_installments == 1) {
 
-                    // Gets the gateway discount.
-                    $credit_discount_x1 = $settings['credit_discount_x1'];
+                    if (isset($settings['credit_discount_x1'])) {
 
-                    $total = ( WC()->cart->cart_contents_total + WC()->cart->shipping_total );
+                        // Gets the gateway discount.
+                        $credit_discount_x1 = $settings['credit_discount_x1'];
 
-                    $discount = $total * ($credit_discount_x1 / 100);
+                        $total = (WC()->cart->cart_contents_total + WC()->cart->shipping_total);
+
+                        $discount = $total * ($credit_discount_x1 / 100);
+                        $cart->add_fee(
+                            sprintf(__('With discount of %s at sight credit card', 'cielo-woocommerce'), $credit_discount_x1 . '%', sanitize_text_field(woocommerce_price($total))),
+                            -$discount
+                        );
+
+                    }
+
+                } elseif ('store' == $settings['installment_type'] && $cielo_credit_installments >= $interest && 0 < $settings['interest_rate']) {
+
+                    $total = (WC()->cart->cart_contents_total + WC()->cart->shipping_total);
+
+                    $interest_rate = (float)$settings['interest_rate'];
+
+                    $interest_subtotal = $total / $cielo_credit_installments;
+
+                    if ($cielo_credit_installments < $settings['installments']) {
+                        $interest_total = $interest_subtotal * ((($interest_rate / 100) + 1) - 0.02);
+                    } else {
+                        $interest_total = $interest_subtotal * ((($interest_rate / 100) + 1) + 0.02);
+                    }
+
+                    $addition = ($interest_total * $cielo_credit_installments) - $total;
+
                     $cart->add_fee(
-                        sprintf(__('With discount of %s at sight credit card', 'cielo-woocommerce'), $credit_discount_x1 . '%', sanitize_text_field(woocommerce_price( $total ))),
-                        -$discount
+                        sprintf(__('With addition of about %s per interest a.m.', 'cielo-woocommerce'), sanitize_text_field(woocommerce_price($interest_total - $interest_subtotal))),
+                        $addition
                     );
 
                 }
 
-            } elseif ('store' == $settings['installment_type'] && $cielo_credit_installments >= $interest && 0 < $settings['interest_rate'] ) {
+            }
 
-                $total = ( WC()->cart->cart_contents_total + WC()->cart->shipping_total );
+        } elseif (WC()->session->chosen_payment_method == 'cielo_debit') {
 
-                $interest_rate = (float) $settings['interest_rate'];
+            $settings = get_option('woocommerce_' . WC()->session->chosen_payment_method . '_settings');
 
-                $interest_subtotal = $total / $cielo_credit_installments;
+            if (isset($settings['debit_discount'])) {
 
-                if ( $cielo_credit_installments < $settings['installments'] ) {
-                    $interest_total = $interest_subtotal * ( ( ( $interest_rate / 100 ) + 1 ) - 0.02 );
-                } else {
-                    $interest_total = $interest_subtotal * ( ( ( $interest_rate / 100 ) + 1 ) + 0.02 );
+                // Gets the gateway discount.
+                $debit_discount = (float) $settings['debit_discount'];
+
+                if (0 < $debit_discount) {
+
+                    $total = (WC()->cart->cart_contents_total + WC()->cart->shipping_total);
+
+                    $discount = $total * ($debit_discount / 100);
+                    $cart->add_fee(
+                        sprintf(__('With discount of %s at debit card', 'cielo-woocommerce'), $debit_discount . '%', sanitize_text_field(woocommerce_price($total))),
+                        -$discount
+                    );
+
                 }
-
-                $addition = ($interest_total * $cielo_credit_installments) - $total;
-
-                $cart->add_fee(
-                    sprintf( __( 'With addition of about %s per interest a.m.', 'cielo-woocommerce' ), sanitize_text_field(woocommerce_price( $interest_total - $interest_subtotal ) ) ),
-                    $addition
-                );
 
             }
 
